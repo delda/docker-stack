f8ab7f2fd47625a49124a70807fa79256906110b
Davide Dell'Erba <delda@virgilio.it>
2017-05-25 18:20:36 +0200

    Use .env variables in Dockerfile

--- containers/apache-2.4/script/docker-entrypoint.sh
+++ containers/apache-2.4/script/docker-entrypoint.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-echo "*** ${PHP_DOCUMENT_ROOT}"
-sed -i "s/%%%DOCUMENT_ROOT%%%/${PHP_DOCUMENT_ROOT}/g" /etc/apache2/sites-available/000-default.conf
-sed -i "s/%%%IP%%%/${PHP_IP}/g" /etc/apache2/sites-available/000-default.conf
+sed -i "s#%%%DOCUMENT_ROOT%%%#${PHP_DOCUMENT_ROOT}#g" /etc/apache2/sites-available/000-default.conf
+sed -i "s#%%%IP%%%#${PHP_IP}#g" /etc/apache2/sites-available/000-default.conf
+
+/usr/sbin/apache2ctl -D FOREGROUND
